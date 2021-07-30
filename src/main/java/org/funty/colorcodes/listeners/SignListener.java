23f2004ba32236789de3c911ead7e52f920bb9c7
FuntyGithub <74513280+FuntyGithub@users.noreply.github.com>
2021-07-30 16:29:46 +0200

    Fixed an issue

--- src/main/java/org/funty/colorcodes/listeners/SignListener.java
+++ src/main/java/org/funty/colorcodes/listeners/SignListener.java
@@ -1,5 +1,6 @@
 package org.funty.colorcodes.listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
@@ -9,10 +10,13 @@ import org.funty.colorcodes.Main;
 public class SignListener implements Listener {
     @EventHandler
     public void onSignChange(SignChangeEvent e) {
-        String[] lines = e.getLines();
-        for(int i = 0; i < 4; i++) {
-            String line = lines[ i ];
-            e.setLine(i, line.replace(Main.getPlugin().getConfig().getString("Settings.ColorCodeCharacter"), "§"));
+        Player p = e.getPlayer();
+        if(Main.getPlugin().getConfig().getString("Settings.InChat.PermissionsSign") == null || p.hasPermission(Main.getPlugin().getConfig().getString("Settings.InChat.PermissionsSign"))){
+            String[] lines = e.getLines();
+            for(int i = 0; i < 4; i++) {
+                String line = lines[ i ];
+                e.setLine(i, line.replace(Main.getPlugin().getConfig().getString("Settings.ColorCodeCharacter"), "§"));
+            }
         }
     }
 }
